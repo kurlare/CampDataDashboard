122061275bce6e6ae619ad6332125d66d5e6e175
Raphael Kurlansik <kurlare@gmail.com>
2015-11-13 13:10:06 -0500

    Second commit

--- server.R
+++ server.R
@@ -5,14 +5,8 @@ library(ggplot2)
 
 
 source("read_clean_campdata.R")
-
-#Plot type selection
-plotType <- function(x, type) {
-    switch(type,
-           D = hist(x),
-           B = barplot(x),
-           C = pie(x))
-}
+campdata <- campdata[sample(nrow(campdata)),]
+reduced <- reduced[sample(nrow(campdata)),]
 
 # Define server logic 
 shinyServer(function(input, output) {
@@ -20,35 +14,54 @@ shinyServer(function(input, output) {
     # Return the requested dataset
     datasetInput <- reactive({
         switch(input$dataset,
-               "Full" = campdata,
-               "Reduced" = reduced)
+               "Full" = campdata[, input$show_cols],
+               "Reduced" = reduced[, input$show_cols])
     })
     
-
+    # Generate an HTML table view of the data
+    output$data_table <- renderDataTable({
+        datasetInput()
+        }, filter = 'top')
     
+    # download the filtered data
+    output$downloadData = downloadHandler(filename = 
+                                              paste('campdata-filtered', Sys.Date(), '.csv', sep = ''),
+                                          content = function(file) {
+                                              s = input$data_table_rows_all
+                                              data <- datasetInput()
+                                              data <- data[s,]
+                                              write.csv(data, file)
+                                          })
+    
+    ## Code for regression model
     runRegression <- reactive({
-        summary(lm(as.formula(paste(input$y," ~ ",paste(input$x,collapse="+"))),
+        
+        regmod <- summary(lm(as.formula(paste(input$y," ~ ",paste(input$x,collapse="+"))),
                    datasetInput()))
-    })
-   
-    ## Generate Plot using x and y inputs from UI
-    output$plot1 <- renderPlot({
-        data <- as.data.frame(datasetInput())
-        ggplot(data, aes_string(x = input$x, y = input$y)) +
-            geom_point(aes(color = Name)) + 
-            geom_smooth(method = lm) 
+        regmod
+
     })
     
-    output$plot2 <- renderPlot({
-        data <- as.data.frame(datasetInput())
-        #data$Bunk <- as.factor(data$Bunk)
-        ggplot(data = data, aes_string(x = input$x, y = input$y, color = factor(data$Bunk))) +
-            #geom_boxplot(outlier.colour = 'red', outlier.size = 2.5) + 
-            #geom_point(fill = data$Bunk, alpha = 0.3)
-        geom_point() + facet_grid(. ~ Bunk) + 
-            geom_boxplot(alpha = 0.3, outlier.colour = 'red', outlier.size = 2.5)
+    
+    ## Regression output
+    output$regression <- renderPrint({
+        runRegression()
+        
+        
     })
     
+    ## Plot residuals
+    output$residplot <- renderPlot({
+        data <- as.data.frame(datasetInput())
+        regmod <- summary(lm(as.formula(paste(input$y," ~ ",paste(input$x,collapse="+"))),
+                             datasetInput()))
+        data <- data[, input$y]
+        if(input$residcheck){
+        plot(data, regmod$residuals, xlab = input$y, ylab = 'Residuals')
+            abline(0, 0)                  # the horizon
+        }
+        
+    })
     
     # Generate a summary of the data
     output$summary <- renderPrint({
@@ -56,28 +69,55 @@ shinyServer(function(input, output) {
         summary(data[, -c(1,2,3,4,5)])
         
     })
+
+    ## Generate Plots using x and y inputs from UI
+    output$plot1 <- renderPlot({
+        data <- as.data.frame(datasetInput())
+        ggplot(data, aes_string(x = input$x, y = input$y)) +
+               geom_point(aes(color = factor(Name))) + 
+               geom_smooth(method = lm) 
+    })
     
-    output$regression <- renderPrint({
-        runRegression()
+    ## Boxplot
+    output$plot2 <- renderPlot({
+        data <- as.data.frame(datasetInput())
+
+        ggplot(data = data, aes_string(x = input$x, y = input$y)) +
+               geom_point(aes(color = factor(Name))) + facet_grid(. ~ Bunk) + 
+               geom_boxplot(aes(color = factor(Bunk)),
+                                alpha = 0.3, outlier.colour = 'red', outlier.size = 2.5)
+    })
+    
+    
+    ## Lineplot
+    output$plot3 <- renderPlot({
+        data <- as.data.frame(datasetInput())
+        ggplot(data = data, aes_string(x = input$x, y = input$y)) +
+               geom_line(aes(group = Name, color = factor(Name))) +
+               geom_point(aes(color = factor(Name)))
+    })
+    
+    
+    ## Identify row of outlier
+    output$position <- renderDataTable({
+        nearPoints(datasetInput(), input$plot_click)
+        brushedPoints(datasetInput(), input$plot_brush)
+        
+    }, filter = 'top')
+    
+    output$position2 <- renderDataTable({
+        nearPoints(datasetInput(), input$plot_click)
+        brushedPoints(datasetInput(), input$plot_brush)
+            
+    })
+    
+    output$position3 <- renderDataTable({
+        nearPoints(datasetInput(), input$plot_click)
+        brushedPoints(datasetInput(), input$plot_brush)
         
     })
     
-    # Generate an HTML table view of the data
-    output$data_table <- DT::renderDataTable(datasetInput(), filter = "bottom", 
-                                             class = 'cell-border stripe')
-    
-    # download the filtered data
-    output$downloadData = downloadHandler(filename = 
-        paste('campdata-filtered', Sys.Date(), '.csv', sep = ''),
-        content = function(file) {
-        s = input$data_table_rows_all
-        data <- datasetInput()
-        data <- data[s,]
-        write.csv(data, file)
-    })
-    
-        
     
 
-    
+
 })
--- ui.R
+++ ui.R
@@ -1,54 +1,82 @@
 
-shinyUI(fluidPage(theme = "bootstrap.css",
-    titlePanel(strong("Camp Data Dashboard")),
-    
-    sidebarLayout(
-        sidebarPanel(
-            
-            selectInput("dataset", "Choose a dataset:", 
-                        choices = c("Full", "Reduced")),
-            
-            selectInput("camper", label = "Choose a camper", 
-                        choices = c("All", unique(campdata$Name))
-            ),
-            br(),
-            selectInput("x", label = "Select X Variable", 
-                        choices = c(colnames(campdata)),
-                        selected = "Day"
-            ),
-            
-            selectInput("y", label = "Select Y Variable", 
-                        choices = c(colnames(campdata)),
-                        selected = "Daily_Total_Points_Earned"
-            ),
-           
-            downloadButton('downloadData', 'Download Filtered Data')
-        
-        ),
-        mainPanel(
-            tabsetPanel(type = "tabs", 
-                        tabPanel("Table", DT::dataTableOutput(outputId ="data_table")),
-                        tabPanel("Summary",
-                                 h4("Regression Model"),
-                                 verbatimTextOutput("regression"),
-                                 
-                                 h4("Summary"),
-                                 verbatimTextOutput("summary")),
-                        
-                        tabPanel("Plot",
-                                 radioButtons("plot_type", "Select Plot Type:",
-                                              c("point", "boxplot")),
-                                 conditionalPanel(
-                                     condition = "input.plot_type == 'point'", plotOutput("plot1")),
-                                 conditionalPanel(
-                                     condition = "input.plot_type == 'boxplot'", plotOutput("plot2"))
-                                 
-                                 
-                        
-                        
-            )
-        ))
-    
-    
-)))
 
+shinyUI(fluidPage(theme = "bootstrap.css",
+                  titlePanel(strong("Camp Data Dashboard")),
+                  
+                  sidebarLayout(
+                      sidebarPanel(
+                          
+                          selectInput("dataset", "Choose a dataset:", 
+                                      choices = c("Full", "Reduced")
+                          ),
+                          
+                          #selectInput("camper", label = "Choose a camper:", 
+                                     # choices = c("All", unique(campdata$Name))
+                         # ),
+                          
+                          selectInput("x", label = "Select X Variable:", 
+                                      choices = c(colnames(campdata)),
+                                      selected = "Day"
+                          ),
+                          
+                          selectInput("y", label = "Select Y Variable:", 
+                                      choices = c(colnames(campdata)),
+                                      selected = "Daily_Total_Points_Earned"
+                          ),
+                         
+                          checkboxGroupInput('show_cols', 'Display Columns:',
+                                                                  names(campdata), 
+                                                                  selected = names(campdata)
+                                                                  
+                          ),
+
+                          
+                          downloadButton('downloadData', 'Download Filtered Data')
+                          
+                      ),
+                      mainPanel(
+                          tabsetPanel(type = "tabs", 
+                                      tabPanel("Table", 
+                                               
+                                               dataTableOutput('data_table')),
+                                      
+                                      tabPanel("Plot",
+                                               radioButtons("plot_type", "Select Plot Type:",
+                                                            c("point", "boxplot", "line"),
+                                                            inline = TRUE),
+                                               #actionButton("move_filtered", "Send Filtered Data to Main Table"),
+                                               conditionalPanel(
+                                                   condition = "input.plot_type == 'point'", 
+                                                   plotOutput("plot1",
+                                                              click = "plot_click",
+                                                              brush = "plot_brush"),
+                                                   dataTableOutput('position')),
+                                               conditionalPanel(
+                                                   condition = "input.plot_type == 'boxplot'",
+                                                   plotOutput("plot2",
+                                                              click = "plot_click",
+                                                              brush = "plot_brush"),
+                                                   dataTableOutput('position2')),
+                                               conditionalPanel(
+                                                   condition = "input.plot_type == 'line'",
+                                                   plotOutput("plot3",
+                                                              click = "plot_click",
+                                                              brush = "plot_brush"),
+                                                   dataTableOutput('position3')
+                                               )
+                                               
+                                      ),
+                                      
+                                      tabPanel("Model",
+                                               checkboxInput("residcheck", "Show Residual Plot", FALSE),
+                                               h4("Regression Model"),
+                                               verbatimTextOutput("regression"),
+                                               plotOutput('residplot'),
+                                               h4("Summary"),
+                                               verbatimTextOutput("summary"))
+                                      
+
+                          ))
+                      
+                      
+                  )))
